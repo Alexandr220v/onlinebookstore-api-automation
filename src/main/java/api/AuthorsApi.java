b40e1a040ce7024ca6e4accc741362da4c74bc77
olexandr220v <alexandr220v@gmail.com>
2026-01-30 20:42:33 +0200

    updated pom.xml

--- src/main/java/api/AuthorsApi.java
+++ src/main/java/api/AuthorsApi.java
@@ -3,6 +3,7 @@ package api;
 import dto.Author;
 import dto.Book;
 import dto.ErrorResponse;
+import io.qameta.allure.Step;
 import io.restassured.RestAssured;
 import io.restassured.internal.mapping.GsonMapper;
 import io.restassured.response.Response;
@@ -19,43 +20,51 @@ public class AuthorsApi  extends  BaseApi{
 
     private String AUTHOR_ID = AUTHORS + "/%s";
 
+    @Step("Get all authors")
     public List<Author> getAllAuthors() {
         return get(AUTHORS, HttpStatus.SC_OK).asList(Author.class);
     }
 
+    @Step("Get author by ID: {id}")
     public Author getAuthor(int id) {
         return get(String.format(AUTHOR_ID, id), HttpStatus.SC_OK).as(Author.class);
     }
 
+    @Step("Attempt to get author by ID: {id} (expecting error)")
     public ErrorResponse getAuthorError(int id) {
         return get(String.format(AUTHOR_ID, id), HttpStatus.SC_BAD_REQUEST).as(ErrorResponse.class);
     }
+
+    @Step("Create new author: {author}")
     public Author createAuthor(Author author) {
-        return post(AUTHORS,author, HttpStatus.SC_OK).as(Author.class);
+        return post(AUTHORS, author, HttpStatus.SC_OK).as(Author.class);
     }
 
+    @Step("Attempt to create author (expecting 400 Bad Request): {author}")
     public ErrorResponse createAuthorError(Author author) {
-        return post(AUTHORS,author, HttpStatus.SC_BAD_REQUEST).as(ErrorResponse.class);
+        return post(AUTHORS, author, HttpStatus.SC_BAD_REQUEST).as(ErrorResponse.class);
     }
 
+    @Step("Create author and expect status code {statusCode}")
     public ResponseData createAuthorError(Author author, int statusCode) {
-        return post(AUTHORS,author, statusCode);
+        return post(AUTHORS, author, statusCode);
     }
 
+    @Step("Update author: {author}")
     public Author updateAuthor(Author author) {
-        return put(String.format(AUTHOR_ID, author.getId()),author, HttpStatus.SC_OK).as(Author.class);
+        return put(String.format(AUTHOR_ID, author.getId()), author, HttpStatus.SC_OK).as(Author.class);
     }
-
+    @Step("Update author error: {author}")
     public ErrorResponse updateAuthorError(Author author) {
         return put(String.format(AUTHOR_ID, author.getId()),author, HttpStatus.SC_BAD_REQUEST).as(ErrorResponse.class);
+    }
+
+    @Step("Delete author with ID: {id}")
+    public ResponseData deleteAuthor(int id) {
+        return delete(String.format(AUTHOR_ID, id), HttpStatus.SC_OK);
     }
 
     public ResponseData updateAuthorError(Author author, int statusCode) {
         return put(String.format(AUTHOR_ID, author.getId()),author, statusCode);
     }
-
-    public ResponseData deleteAuthor(int id) {
-        return delete(String.format(AUTHOR_ID, id), HttpStatus.SC_OK);
-    }
-
 }
--- src/main/java/api/BooksApi.java
+++ src/main/java/api/BooksApi.java
@@ -4,6 +4,7 @@ package api;
 import com.google.gson.Gson;
 import dto.Book;
 import dto.ErrorResponse;
+import io.qameta.allure.Step;
 import org.apache.http.HttpStatus;
 
 import java.util.List;
@@ -13,34 +14,42 @@ public class BooksApi extends BaseApi {
     private String BOOKS = "/Books";
     private String BOOK_ID = BOOKS + "/%s";
 
+    @Step("Fetch list of all books")
     public List<Book> getBooksList() {
         return get(BOOKS, HttpStatus.SC_OK).asList(Book.class);
     }
 
+    @Step("Get book details for ID: {id}")
     public Book getBook(int id) {
         return get(String.format(BOOK_ID, id), HttpStatus.SC_OK).as(Book.class);
     }
 
+    @Step("Attempt to get non-existent book ID: {id}")
     public ErrorResponse getBookError(int id) {
         return get(String.format(BOOK_ID, id), HttpStatus.SC_NOT_FOUND).as(ErrorResponse.class);
     }
 
+    @Step("Create a new book: {book}")
     public Book createBook(Book book) {
-        return post(BOOKS,new Gson().toJson(book), HttpStatus.SC_OK).as(Book.class);
+        return post(BOOKS, new Gson().toJson(book), HttpStatus.SC_OK).as(Book.class);
     }
 
+    @Step("Create book and expect error: {book}")
     public ErrorResponse createBookError(Book book) {
-        return post(BOOKS,new Gson().toJson(book), HttpStatus.SC_BAD_REQUEST).as(ErrorResponse.class);
+        return post(BOOKS, new Gson().toJson(book), HttpStatus.SC_BAD_REQUEST).as(ErrorResponse.class);
     }
 
+    @Step("Update book details for ID: {book.id}")
     public Book updateBook(Book book) {
-        return put(String.format(BOOK_ID, book.getId()),new Gson().toJson(book), HttpStatus.SC_OK).as(Book.class);
+        return put(String.format(BOOK_ID, book.getId()), new Gson().toJson(book), HttpStatus.SC_OK).as(Book.class);
     }
 
+    @Step("Update book and expect error: {book}")
     public ErrorResponse updateBookError(Book book) {
-        return put(String.format(BOOK_ID, book.getId()),new Gson().toJson(book), HttpStatus.SC_BAD_REQUEST).as(ErrorResponse.class);
+        return put(String.format(BOOK_ID, book.getId()), new Gson().toJson(book), HttpStatus.SC_BAD_REQUEST).as(ErrorResponse.class);
     }
 
+    @Step("Delete book with ID: {id}")
     public ResponseData deleteBook(int id) {
         return delete(String.format(BOOK_ID, id), HttpStatus.SC_OK);
     }
